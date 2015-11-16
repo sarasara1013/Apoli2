ef68412826ca7d593ac437815044d2cf985c1aae
reina <sarasara1013@gmail.com>
2015-11-16 22:26:44 +0900

    AppoSelectViewControllerの追加1

--- yoteikakunin/Controllers/Check/AppoSelectViewController.swift
+++ yoteikakunin/Controllers/Check/AppoSelectViewController.swift
@@ -10,27 +10,13 @@ import UIKit
 
 class AppoSelectViewController: UIViewController {
     
-    //相手のプロフィール画像、日時を表示
-    
-    //parseは保留どうすればいいかまっすーに聞く
-    //画像名前時間をます登録してcommit
-
     
     @IBOutlet var userImageView: UIImageView!
     @IBOutlet weak var name: UILabel!
     @IBOutlet var appoTableView: UITableView!
     
     
-    
-    @IBAction func time(sender: AnyObject) {
-        
-        
-    }
- 
-    
-    
-    
-    
+
     //通知がきたら一番上にくる
     
     override func viewDidLoad() {
